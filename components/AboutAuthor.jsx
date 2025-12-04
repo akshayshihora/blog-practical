f696a69c636e32723465ec839985eb60ed25974b
akshayshihora <shihoraa0@gmail.com>
2025-12-04 17:49:51 +0530

    change

--- components/AboutAuthor.jsx
+++ components/AboutAuthor.jsx
@@ -2,7 +2,7 @@ export default function AboutAuthor({ author }) {
   return (
     <div className="about-center">
       <h2 className="about-center-title">About {author}</h2>
-      <img src="/alex.png" className="about-center-img" />
+      <img src="/Alex.png" className="about-center-img" />
 
       <p className="about-center-text">
         With over a decade in fitness, Alex specializes in strength training.
